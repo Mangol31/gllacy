dc4a06f9f5d11a2b725c91ad58a8cfccac828315
Mangol <tritemy@yandex.ru>
2018-06-06 20:39:50 +0500

    Сверстан и стилизован хедэр для главной страницы и каталога

--- js/modal-enter.js
+++ js/modal-enter.js
@@ -1,10 +1,24 @@
 var modalWindowEnter = document.querySelector(".modal-window-enter");
+var modalWindowSearch = document.querySelector(".modal-window-search");
+var modalCartOrder = document.querySelector(".modal-window-order");
+
 var enterButton = document.querySelector(".search-enter-cart_item__enter-button");
+
+var searchButton = document.querySelector(".search-enter-cart_item__search-button");
+
+var cartButton = document.querySelector(".search-enter-cart_item__cart-button");
 
 enterButton.addEventListener("click", function (evt) {
     evt.preventDefault();
-    modalWindowEnter.classList.remove("visually-hidden");
+    modalWindowEnter.classList.toggle("visually-hidden");
 });
 
+searchButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modalWindowSearch.classList.toggle("visually-hidden");
+});
 
-
+cartButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modalCartOrder.classList.toggle("visually-hidden");
+});
