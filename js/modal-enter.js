f7438bc9e9b491bc52953d7d43558fadffdcde28
Mangol <tritemy@yandex.ru>
2018-06-28 16:13:33 +0500

    Доделал БЭМ в хэдере и остальные косяки. По-моему хэдер готов

--- js/modal-enter.js
+++ js/modal-enter.js
@@ -2,11 +2,11 @@ var modalWindowEnter = document.querySelector(".modal-window-enter");
 var modalWindowSearch = document.querySelector(".modal-window-search");
 var modalCartOrder = document.querySelector(".modal-window-order");
 
-var enterButton = document.querySelector(".search-enter-cart_item__enter-button");
+var enterButton = document.querySelector(".secondary-menu_item__enter-button");
 
-var searchButton = document.querySelector(".search-enter-cart_item__search-button");
+var searchButton = document.querySelector(".search-button");
 
-var cartButton = document.querySelector(".search-enter-cart_item__cart-button");
+var cartButton = document.querySelector(".cart-button");
 
 enterButton.addEventListener("click", function (evt) {
     evt.preventDefault();
